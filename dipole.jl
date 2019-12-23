002c40f5db66a154cccb1df8fd8378fb0084fa1b
Rodney Polkinghorne <rpolkinghorne@swin.edu.au>
2019-12-23 15:28:58 +1100

    Relax the TF oprm by SOR

--- dipole.jl
+++ dipole.jl
@@ -17,7 +17,9 @@ z = reshape(grid,1,1,:)
 
 xyplane(u) = heatmap(y[:], x[:], u[:,:,(Nz+1)÷2], xlabel="y", ylabel="x")
 xzplane(u) = heatmap(z[:], x[:], u[:,(Nxy+1)÷2,:], xlabel="z", ylabel="x")
-∫(u) = hz*hxy^2*sum(u)
+
+dR = hz*hxy^2
+∫(u) = dR*sum(u)
 
 V = (abs2.(x) .+ abs2.(y) .+ abs2.(z)/69.91)/2
 
@@ -33,9 +35,7 @@ function op(N, stencil)
     diags = [i-mid=>fill(stencil[i],N-abs(i-mid)) for i = keys(stencil)]
     BandedMatrix(Tuple(diags), (N,N))
 end
-∂ = (1/hxy).*op(Nxy, [-1/60, 3/20, -3/4, 0, 3/4, -3/20, 1/60])
 ∂² = (1/hxy^2).*op(Nxy, [1/90, -3/20, 3/2, -49/18, 3/2, -3/20, 1/90])
-∂z = (1/hz).*op(Nz, [-1/60, 3/20, -3/4, 0, 3/4, -3/20, 1/60])
 ∂²z = (1/hz^2).*op(Nz, [1/90, -3/20, 3/2, -49/18, 3/2, -3/20, 1/90])
 
 function contract(A,x,dim)
@@ -49,11 +49,11 @@ end
 # test contraction
 
 for (j,u,uu) = [(1,x,xx), (2,y,yy)]
-    @assert contract(∂,u,j)[:] ≈ ∂*u[:]
-    @assert all(contract(∂,uu,j) .≈ contract(∂,u,j))
+    @assert contract(∂²,u,j)[:] ≈ ∂²*u[:]
+    @assert all(contract(∂²,uu,j) .≈ contract(∂²,u,j))
 end
-@assert contract(∂z,z,3)[:] ≈ ∂z*z[:]
-@assert all(contract(∂z,zz,3) .≈ contract(∂z,z,3))
+@assert contract(∂²z,z,3)[:] ≈ ∂²z*z[:]
+@assert all(contract(∂²z,zz,3) .≈ contract(∂²z,z,3))
 
 # Thomas-Fermi oprm to find healing length and grid size
 μ₀ = 0.0
@@ -75,32 +75,28 @@ end
 μ = μ₀ + (μ₁-μ₀)/2
 
 # solve by SOR: 
-# -∂²*ψ-ψ*∂²+V.*ψ+C*abs2.(ψ).*ψ = μ*ψ
+# -∇²*ψ/2-ψ*∂²+V.*ψ+C*abs2.(ψ).*ψ = μ*ψ
 
-# ψ₀ = similar(ψ)
-# nnc = zero(ψ)	# left in for future use
-# potential = []
-# residual = []
-# @time for _ = 1:Int(1000)
-#     ψ₀ .= ψ
-#     for k = keys(ψ)
-#         i,j = Tuple(k)
-#         ψ[k] = 0
-#         T = ∂²[i:i,:]*ψ[:,j:j]+ψ[i:i,:]*∂²[:,j:j]
-#         L = y[i]*(ψ[i:i,:]*∂'[:,j:j])-x[j]*(∂[i:i,:]*ψ[:,j:j])
-#         ψk = (μ*ψ₀[k]+T[]+1im*Ω*L[]) /
-#             (-2*∂²[1,1]+V[k]+C*(abs2.(ψ₀[k])+2nnc[k]))
-#         ψ[k] = ψ₀[k] + a*(ψk-ψ₀[k])
-#         impose_vortex!(ψ)
-#      end
-#      Lψ = -∂²*ψ-ψ*∂²+V.*ψ+C*(abs2.(ψ₀)+2nnc).*ψ-1im*Ω*(y.*(ψ*∂')-x.*(∂*ψ))
-#      # take residual of unconstrained components
-#      impose_vortex!(Lψ)
-#      E = sum(conj.(ψ).*Lψ)/norm(ψ)^2 |> real
-#      push!(potential, E)
-#      push!(residual, norm(Lψ-E*ψ)/norm(ψ))
-# end
-# 
+ψ = copy(TFψ)
+ψ₀ = similar(ψ)
+residual = []
+for _ = 1:100
+    ψ₀ .= ψ
+    for R = keys(ψ)
+        i,j,k = Tuple(R)
+        ψ[R] = 0
+        T = (∂²[i,:]⋅ψ[:,j,k]+∂²[j,:]⋅ψ[i,:,k]+∂²z[k,:]⋅ψ[i,j,:]) / 2
+        ψr = (μ*ψ₀[R]+T) / (-∂²[1,1,1]-∂²z[1,1,1]/2+V[R]+g*(abs2.(ψ₀[R])))
+        ψ[R] = ψ₀[R] + a*(ψr-ψ₀[R])
+     end
+     Lψ = -(contract(∂²,ψ,1)+contract(∂²,ψ,2)+contract(∂²z,ψ,3))/2 +
+         V.*ψ + g*(abs2.(ψ)).*ψ
+     E = sum(conj.(ψ).*Lψ)/norm(ψ)^2 |> real
+     push!(residual, norm(Lψ-E*ψ)/norm(ψ))
+end
+
+# scatter(residual, mc=:black, ms=2, yscale=:log10, leg=:none)
+
 # # propagate
 # 
 # P = ODEProblem((ψ,p,t)->-1im*(-∂²*ψ-ψ*∂²+V.*ψ+C*abs2.(ψ).*ψ-1im*Ω*(y.*(ψ*∂')-x.*(∂*ψ))), ψ, (0.0,10.0))
