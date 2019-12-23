a7f2504c8ce0d1ecbf1aa7462f619c6c61937151
Rodney Polkinghorne <rpolkinghorne@swin.edu.au>
2019-12-23 15:43:57 +1100

    Lengthen x axis for oscillations

--- dipole.jl
+++ dipole.jl
@@ -5,21 +5,22 @@ using Plots, ComplexPhasePortrait, Printf
 
 N = 1e5		# number of atoms
 g = 0.06194	# repulsion constant
-hxy = 0.5;  Nxy = 21
+
+hx = 0.5;  Nx = 35
+hy = 0.5;  Ny = 21
 hz = 5; Nz = 17
 a = 1.4		# SOR polation
 
-grid = hxy/2*(1-Nxy:2:Nxy-1) |> SVector{Nxy}
-x = reshape(grid,:,1,1)
-y = reshape(grid,1,:,1)
-grid = hz/2*(1-Nz:2:Nz-1) |> SVector{Nz}
-z = reshape(grid,1,1,:)
+grid(h,N) = h/2*(1-N:2:N-1) |> SVector{N}
+x = reshape(grid(hx,Nx),:,1,1)
+y = reshape(grid(hy,Ny),1,:,1)
+z = reshape(grid(hz,Nz),1,1,:)
 
 xyplane(u) = heatmap(y[:], x[:], u[:,:,(Nz+1)÷2], xlabel="y", ylabel="x")
-xzplane(u) = heatmap(z[:], x[:], u[:,(Nxy+1)÷2,:], xlabel="z", ylabel="x")
+xzplane(u) = heatmap(z[:], x[:], u[:,(Ny+1)÷2,:], xlabel="z", ylabel="x")
 
-dR = hz*hxy^2
-∫(u) = dR*sum(u)
+H = hx*hy*hz
+∫(u) = H*sum(u)
 
 V = (abs2.(x) .+ abs2.(y) .+ abs2.(z)/69.91)/2
 
@@ -35,7 +36,8 @@ function op(N, stencil)
     diags = [i-mid=>fill(stencil[i],N-abs(i-mid)) for i = keys(stencil)]
     BandedMatrix(Tuple(diags), (N,N))
 end
-∂² = (1/hxy^2).*op(Nxy, [1/90, -3/20, 3/2, -49/18, 3/2, -3/20, 1/90])
+∂²x = (1/hx^2).*op(Nx, [1/90, -3/20, 3/2, -49/18, 3/2, -3/20, 1/90])
+∂²y = (1/hy^2).*op(Ny, [1/90, -3/20, 3/2, -49/18, 3/2, -3/20, 1/90])
 ∂²z = (1/hz^2).*op(Nz, [1/90, -3/20, 3/2, -49/18, 3/2, -3/20, 1/90])
 
 function contract(A,x,dim)
@@ -47,11 +49,10 @@ function contract(A,x,dim)
 end
 
 # test contraction
-
-for (j,u,uu) = [(1,x,xx), (2,y,yy)]
-    @assert contract(∂²,u,j)[:] ≈ ∂²*u[:]
-    @assert all(contract(∂²,uu,j) .≈ contract(∂²,u,j))
-end
+@assert contract(∂²x,x,1)[:] ≈ ∂²x*x[:]
+@assert all(contract(∂²x,xx,1) .≈ contract(∂²x,x,1))
+@assert contract(∂²y,y,2)[:] ≈ ∂²y*y[:]
+@assert all(contract(∂²y,yy,2) .≈ contract(∂²y,y,2))
 @assert contract(∂²z,z,3)[:] ≈ ∂²z*z[:]
 @assert all(contract(∂²z,zz,3) .≈ contract(∂²z,z,3))
 
@@ -80,16 +81,17 @@ end
 ψ = copy(TFψ)
 ψ₀ = similar(ψ)
 residual = []
+diagel = -(∂²x[1,1,1]+∂²y[1,1,1]+∂²z[1,1,1])/2
 for _ = 1:100
     ψ₀ .= ψ
     for R = keys(ψ)
         i,j,k = Tuple(R)
         ψ[R] = 0
-        T = (∂²[i,:]⋅ψ[:,j,k]+∂²[j,:]⋅ψ[i,:,k]+∂²z[k,:]⋅ψ[i,j,:]) / 2
-        ψr = (μ*ψ₀[R]+T) / (-∂²[1,1,1]-∂²z[1,1,1]/2+V[R]+g*(abs2.(ψ₀[R])))
+        T = (∂²x[i,:]⋅ψ[:,j,k]+∂²y[j,:]⋅ψ[i,:,k]+∂²z[k,:]⋅ψ[i,j,:]) / 2
+        ψr = (μ*ψ₀[R]+T) / (diagel+V[R]+g*(abs2.(ψ₀[R])))
         ψ[R] = ψ₀[R] + a*(ψr-ψ₀[R])
      end
-     Lψ = -(contract(∂²,ψ,1)+contract(∂²,ψ,2)+contract(∂²z,ψ,3))/2 +
+     Lψ = -(contract(∂²x,ψ,1)+contract(∂²y,ψ,2)+contract(∂²z,ψ,3))/2 +
          V.*ψ + g*(abs2.(ψ)).*ψ
      E = sum(conj.(ψ).*Lψ)/norm(ψ)^2 |> real
      push!(residual, norm(Lψ-E*ψ)/norm(ψ))
